ed053e2e29407167313f0634530cbbf6bd3b129e
Elizabeth Shulman <ers58@duke.edu>
2018-02-20 23:05:18 -0500

    initial controlpanel work begun

--- src/visual_elements/ControlPanel.java
+++ src/visual_elements/ControlPanel.java
@@ -1,5 +1,16 @@
 package visual_elements;
 
-public class ControlPanel {
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 
+public class ControlPanel extends GridPane {
+
+	public ControlPanel() {
+		
+		this.add(new Button(), 0, 0);
+		this.add(new Button(), 0, 1);
+		this.add(new Button(), 0, 2);
+	
+	}
+	
 }
