45e6973e453114b175820ca5fb9d1f413b629c4d
Shicheng Rao <sr307@duke.edu>
2018-02-20 20:58:43 -0500

    my classes, somewhat(?) working implementation

--- src/variables/Variable.java
+++ src/variables/Variable.java
@@ -1,5 +1,25 @@
 package variables;
-
+// class exists in case of need for string/int/array variables. For now is only for doubles
 public class Variable {
+	private double value;
+	
+	public Variable(Object data) {
+		value = (double) data;
+	}
+	// gives the type of the current variable
+    public String getType() {
+    		return "Double";
+    }
+
+    // returns the stored value 
+    public Object getValue() {
+    		return value;
+    }
+
+    //changes value to new value, throws TypeException if the type doesn't match the variable type. 
+    public void setValue(Object newValue){
+    	//TODO throw exception
+    		value = (double) newValue;
+    }
 
 }
--- src/variables/VariableManager.java
+++ src/variables/VariableManager.java
@@ -1,5 +1,33 @@
 package variables;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VariableManager {
+	private Map<String, Variable> variables;
+	
+	public VariableManager() {
+		variables = new HashMap<>();
+	}
+	//returns the variable with a given name, or NoSuchVariableException if it doesn't exist.
+    public Variable getVariable(String name) {
+    		//TODO: handle exception
+    		return variables.get(name);
+    }
+
+    //adds a new variable. returns DuplicatedVariableException if the variable already exists
+    public void addVariable(Variable var, String name) {
+    		variables.put(name, var);
+    }
+
+    //changes the variable that matches the name
+    public void setVariable(Object data, String name) {
+    		variables.replace(name, new Variable(data));
+    }
+
+    // this method returns a class type that is used to create variables, no need for now
+//    public Class<?> createVariable(){
+//    		
+//    }
 
 }
