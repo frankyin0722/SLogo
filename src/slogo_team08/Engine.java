08572613e7b4f3bc6cce21a452f12b54495b601f
xy72 <xy72@duke.edu>
2018-02-20 23:40:49 -0500

    Added turtle

--- src/slogo_team08/Engine.java
+++ src/slogo_team08/Engine.java
@@ -10,8 +10,8 @@ public class Engine {
 	public Group myRoot;
 	private Scene myScene;
 	
-	public static final int INITIAL_SCENE_WIDTH = 800;
-	public static final int INITIAL_SCENE_HEIGHT = 800;
+	public static final int INITIAL_SCENE_WIDTH = 1000;
+	public static final int INITIAL_SCENE_HEIGHT = 600;
 	
 	public void initializeSimulation(Stage primaryStage) {
 		myStage = primaryStage;
--- src/visual_elements/DrawingWindow.java
+++ src/visual_elements/DrawingWindow.java
@@ -1,7 +1,11 @@
 package visual_elements;
 
+import java.io.File;
+
 import javafx.geometry.Insets;
 import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
@@ -13,12 +17,19 @@ import turtle.Turtle;
 public class DrawingWindow extends Pane {
 	public static final int INITIAL_WIDTH = 600;
 	public static final int INITIAL_HEIGHT = 400;
+	public static final int TURTLE_WIDTH = 20;
+	public static final int TURTLE_HEIGHT = 30;
+
+//	public static final String IMAGE_PATH = "./images/";
+	public static final String FRANKLIN_IMAGE = "franklin.jpg";
+	public static final String TURTLE_IMAGE = "cute_turtle.png";
+//	public static final String TEST_IMAGE = "./images/cute_turtle.png";
 	private Turtle myTurtle;
 
 	public DrawingWindow(Group root) {
 		setupInitialCanvas();
 		
-		this.getChildren().addAll(testing());
+		this.getChildren().addAll(setupTurtle());
 		root.getChildren().addAll(this);
 	}
 	
@@ -36,8 +47,19 @@ public class DrawingWindow extends Pane {
 		return rect;
 	}
 	
-//	private void setupTurtle(Group root) {
-//		
-//	}
+	private Turtle setupTurtle() {
+
+		Image turtleImage = new Image(getClass().getClassLoader().getResourceAsStream(TURTLE_IMAGE));
+//		Image turtleImage = new Image(getClass().getClassLoader().getResourceAsStream(TEST_IMAGE));
+
+		Turtle turtle = new Turtle();
+		turtle.setImage(turtleImage);
+		turtle.setFitWidth(TURTLE_WIDTH);
+		turtle.setFitHeight(TURTLE_HEIGHT);
+		turtle.setX((INITIAL_WIDTH - TURTLE_WIDTH)/2);
+		turtle.setY((INITIAL_HEIGHT - TURTLE_HEIGHT)/2);
+
+		return turtle;
+	}
 	
 }
