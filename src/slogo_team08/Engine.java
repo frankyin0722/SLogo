4486aab4723b5b80f0a72e5eb4839b1cd16c0971
Elizabeth Shulman <ers58@duke.edu>
2018-02-20 20:51:41 -0500

    Completed Main class

--- src/slogo_team08/Engine.java
+++ src/slogo_team08/Engine.java
@@ -1,5 +1,15 @@
 package slogo_team08;
 
+import javafx.stage.Stage;
+
 public class Engine {
 
+	Stage myStage;
+	
+	public void initializeSimulation(Stage primaryStage) {
+
+		myStage = primaryStage;
+		
+	}
+	
 }
--- src/slogo_team08/Main.java
+++ src/slogo_team08/Main.java
@@ -1,5 +1,24 @@
 package slogo_team08;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+/**
+ * @author benhubsch
+ * @author elizabethshulman
+ * @author andrewyeung
+ * 
+ * This class launches the IDE.
+ */
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Engine e = new Engine();
+		e.initializeSimulation(primaryStage);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
