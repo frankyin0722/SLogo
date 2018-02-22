9a857ea3fefd52ca8013ce4a05b5b4aa9011a6f7
xy72 <xy72@duke.edu>
2018-02-22 17:17:33 -0500

    Added API_Review_xy72

--- src/visual_elements/DrawingWindow.java
+++ src/visual_elements/DrawingWindow.java
@@ -1,59 +1,68 @@
 package visual_elements;
 
-import java.io.File;
 
 import javafx.geometry.Insets;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import turtle.Turtle;
 
 public class DrawingWindow extends Pane {
-	public static final int INITIAL_WIDTH = 800;
-	public static final int INITIAL_HEIGHT = 400;
-	public static final int TURTLE_WIDTH = 20;
+	public static final int INITIAL_WIDTH = 900;
+	public static final int INITIAL_HEIGHT = 500;
+	public static final int TURTLE_WIDTH = 25;
 	public static final int TURTLE_HEIGHT = 30;
 
 //	public static final String IMAGE_PATH = "./images/";
 	public static final String FRANKLIN_IMAGE = "franklin.jpg";
 	public static final String TURTLE_IMAGE = "cute_turtle.png";
-//	public static final String TEST_IMAGE = "./images/cute_turtle.png";
+//	public static final String TEST_IMAGE = "./src/images/cute_turtle.png";
 	private Turtle myTurtle;
+	private double myHomeX;
+	private double myHomeY;
 
 	public DrawingWindow() {
 		setupInitialCanvas();
-		setupTurtle();
+		getNewTurtle();
+//		setupTurtle();
+		
 //		myTurtle.changeX(myTurtle.getX() + 100);
 		myTurtle.setDirection(Math.PI/2);
-//		this.getChildren().addAll(testing());
-//		root.getChildren().addAll(this);
 	}
 	
 	private void setupInitialCanvas() {
 		this.setPrefSize(INITIAL_WIDTH, INITIAL_HEIGHT);
+		this.setMaxSize(INITIAL_WIDTH, INITIAL_HEIGHT);
 		this.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
 	}
 	
-	private Rectangle testing() {
+	private void testing() {
 		Rectangle rect = new Rectangle(20, 20, 100, 100);
 		rect.setFill(Color.AQUAMARINE);
-		return rect;
+		this.getChildren().add(rect);
 	}
 	
 	private void setupTurtle() {
+		StackPane sp = new StackPane();
+//		sp.getChildren().add(myTurtle);
+		sp.setAlignment(Pos.CENTER);
+//		sp.translateXProperty().bind(this.widthProperty().subtract(sp.widthProperty()).divide(2));
+//		sp.translateYProperty().bind(this.heightProperty().subtract(sp.heightProperty()).divide(2));
+		this.getChildren().add(sp);
+	}
+	
+	private void getNewTurtle() {
 		Image turtleImage = new Image(getClass().getClassLoader().getResourceAsStream(TURTLE_IMAGE));
 //		Image turtleImage = new Image(getClass().getClassLoader().getResourceAsStream(TEST_IMAGE));
 		myTurtle = new Turtle(turtleImage);
 		myTurtle.setFitWidth(TURTLE_WIDTH);
 		myTurtle.setFitHeight(TURTLE_HEIGHT);
-//		myTurtle.changeX((this.getMaxWidth() - TURTLE_WIDTH)/2);
-//		myTurtle.changeY((this.getHeight() - TURTLE_HEIGHT)/2);
 		myTurtle.changeX((INITIAL_WIDTH - TURTLE_WIDTH)/2);
 		myTurtle.changeY((INITIAL_HEIGHT - TURTLE_HEIGHT)/2);
 		this.getChildren().add(myTurtle);
