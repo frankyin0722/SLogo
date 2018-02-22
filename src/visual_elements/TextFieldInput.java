6eefc4bcf94e60f6d7b0a93861ed4705234d9d36
Elizabeth Shulman <ers58@duke.edu>
2018-02-22 18:58:14 -0500

    EventHandler initiated for Run button

--- src/visual_elements/TextFieldInput.java
+++
@@ -1,7 +0,0 @@
-package visual_elements;
-
-import javafx.scene.control.TextField;
-
-public class TextFieldInput extends TextField {
-
-}
--- src/visual_elements/Visualization.java
+++ src/visual_elements/Visualization.java
@@ -3,6 +3,14 @@ package visual_elements;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
+/**
+ * @author elizabethshulman
+ * @author xlany
+ * @param scene
+ * 
+ * Initializes and arranges each of the elements within the scene
+ * 
+ */
 public class Visualization {
 	
 	public static final int INITIAL_SCENE_WIDTH = 1000;
@@ -10,14 +18,6 @@ public class Visualization {
 	
 	private Scene myScene;
 	private BorderPane myPane;
-	/**
-	 * @author elizabethshulman
-	 * @author xlany
-	 * @param scene
-	 * 
-	 * Initializes and arranges each of the elements within the scene
-	 * 
-	 */
 
 	public Visualization() {
 		myPane = new BorderPane();
@@ -25,12 +25,13 @@ public class Visualization {
 	}
 
 	private void initializeVis() {
+		myScene = new Scene(myPane,INITIAL_SCENE_WIDTH,INITIAL_SCENE_HEIGHT);
+
 		myPane.setCenter(new DrawingWindow());
-		myPane.setBottom(new TextFieldInput());
+		myPane.setBottom(new ControlPanelConsole());
 		myPane.setRight(new ControlPanelRight());
 		myPane.setLeft(new ControlPanelLeft());
 		
-		myScene = new Scene(myPane,INITIAL_SCENE_WIDTH,INITIAL_SCENE_HEIGHT);
 	}
 	
 	public Scene getScene() {
