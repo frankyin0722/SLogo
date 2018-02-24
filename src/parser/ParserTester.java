570f2915d6fe7a2914a1903ccea8b399dcbe6b65
Shicheng Rao <sr307@duke.edu>
2018-02-23 19:16:59 -0500

    ignore this

--- src/parser/ParserTester.java
+++ src/parser/ParserTester.java
@@ -2,7 +2,7 @@ package parser;
 
 public class ParserTester {
 	public static void main (String[] args) {
-		String userinput = "fd 50 bw 50";
+		String userinput = "setxy 50 50";
 		Parser myparser = new Parser();
 		String language = "resources.languages/English";
 		
