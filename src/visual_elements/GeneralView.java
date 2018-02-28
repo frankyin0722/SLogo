402b8a4237cad0c5ec804628ac504077402478a3
xy72 <xy72@duke.edu>
2018-02-28 00:31:15 -0500

    Added help menu that links to website

--- src/visual_elements/GeneralView.java
+++
@@ -1,9 +0,0 @@
-package visual_elements;
-
-import buttons.ChineseButton;
-import buttons.EnglishButton;
-
-public interface GeneralView {
-	EnglishButton myEnglishButton = new EnglishButton();
-	ChineseButton myChineseButton = new ChineseButton();
-}
