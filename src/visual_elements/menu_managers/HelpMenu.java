b7fcf45c1e8b2bcbabd2a2df389ec919255c3672
xy72 <xy72@duke.edu>
2018-02-28 00:02:24 -0500

    can run

---
+++ src/visual_elements/menu_managers/HelpMenu.java
@@ -0,0 +1,18 @@
+package visual_elements.menu_managers;
+
+import javafx.scene.control.Button;
+
+public class HelpMenu {
+	public HelpMenu() {
+		setupHelpMenu();
+	}
+	
+	private void setupHelpMenu() {
+		
+	}
+	
+	private void setupWebLink() {
+		Button commands = new Button();
+//		commands.
+	}
+}
