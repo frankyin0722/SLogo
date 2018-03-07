bd64edeb3f84238f6c1aeecd0bf1bacd7e1431b8
Elizabeth Shulman <ers58@duke.edu>
2018-03-07 11:50:33 -0500

    Pre-pull commit

--- src/visual_elements/ScrollingDrawingWindow.java
+++ src/visual_elements/ScrollingDrawingWindow.java
@@ -3,7 +3,7 @@ package visual_elements;
 import javafx.scene.control.ScrollPane;
 import turtle.Turtle;
 
-public class ScrollingDrawingWindow extends ScrollPane{
+public class ScrollingDrawingWindow extends ScrollPane {
 	private DrawingWindow myDrawingWindow;
 	public ScrollingDrawingWindow() {
 		myDrawingWindow = new DrawingWindow();
@@ -14,14 +14,7 @@ public class ScrollingDrawingWindow extends ScrollPane{
 		this.setContent(myDrawingWindow);
 		this.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 		this.setHbarPolicy(ScrollBarPolicy.ALWAYS);
-//		this.setPrefViewportWidth(100);
-//		this.setPrefViewportHeight(100);
-		
 	}
-		
-//	private void setupScrolling() {
-//	
-//	}
 	
 	public Turtle getDefaultTurtle() {
 		return myDrawingWindow.getDefaultTurtle();
