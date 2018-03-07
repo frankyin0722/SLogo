f9d380114f491e3a86321a574cba010d60bfdf7c
Elizabeth Shulman <ers58@duke.edu>
2018-03-07 13:50:24 -0500

    turtle pane extended size????

--- src/view/ScrollingDrawingWindow.java
+++ src/view/ScrollingDrawingWindow.java
@@ -4,7 +4,11 @@ import javafx.scene.control.ScrollPane;
 import turtle.Turtle;
 
 public class ScrollingDrawingWindow extends ScrollPane {
+	
 	private DrawingWindow myDrawingWindow;
+	public static final double INITIAL_WIDTH = 695;
+	public static final double INITIAL_HEIGHT = 500;
+	
 	public ScrollingDrawingWindow() {
 		myDrawingWindow = new DrawingWindow();
 		setupScrollingWindow();
@@ -14,6 +18,9 @@ public class ScrollingDrawingWindow extends ScrollPane {
 		this.setContent(myDrawingWindow);
 		this.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 		this.setHbarPolicy(ScrollBarPolicy.ALWAYS);
+		myDrawingWindow.setPrefSize(5000,5000);
+		myDrawingWindow.setManaged(false);
+		this.setPrefSize(INITIAL_WIDTH, INITIAL_HEIGHT);
 	}
 	
 	public Turtle getDefaultTurtle() {
