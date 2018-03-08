c06171fda8a03cc442bba8c0447124f3bdce147d
Elizabeth Shulman <ers58@duke.edu>
2018-03-08 10:16:23 -0500

    retrying javadoc hyperlink

--- src/view/vis_elements/TabDragFunctionality.java
+++ src/view/vis_elements/TabDragFunctionality.java
@@ -1,11 +1,9 @@
 package view.vis_elements;
 
 
+
 /**
- * Add tab reordering functionality to a TabPane tab.
- * Modeled after <a href="https://stackoverflow.com/questions/41473987/how-to-drag-and-drop-tabs-of-the-same-tabpane">this StackOverflow post</a>
- * @author elizabethshulman
- *
+ * See <a href="http://google.com">http://google.com</a>
  */
 public class TabDragFunctionality {
 
