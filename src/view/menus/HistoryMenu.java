4f89c26ee868914f2127463b55506289f70c9fc8
Elizabeth Shulman <ers58@duke.edu>
2018-03-08 01:56:54 -0500

    Repeat History commands

--- src/view/menus/HistoryMenu.java
+++ src/view/menus/HistoryMenu.java
@@ -3,22 +3,33 @@ package view.menus;
 import java.util.ResourceBundle;
 
 import interpreter.CommandTreeInterpreter;
-import javafx.beans.InvalidationListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
+import javafx.scene.input.MouseEvent;
 import observables.Listener;
+import parser.Parser;
 
+/**
+ * yikes languages can't be changed here
+ * @author elizabethshulman
+ *
+ */
 public class HistoryMenu extends TitledPane implements Listener {
+	
 	public static final String HISTORY_KEY = "History";
 	private ListView<String> historyDisplay;
 	private CommandTreeInterpreter interpreter;
+	private ResourceBundle myResources;
 	
 	public HistoryMenu(CommandTreeInterpreter i, ResourceBundle resources) {
 		historyDisplay = new ListView<String>();
 		interpreter = i;
 		interpreter.addListener(this);
+		myResources = resources;
+		callHistoryOnClick();
 		
 		this.setText(resources.getString(HISTORY_KEY));
 		this.setExpanded(false);
@@ -30,6 +41,19 @@ public class HistoryMenu extends TitledPane implements Listener {
 		ObservableList<String> history =FXCollections.observableArrayList (
 			    interpreter.getHistory());
 		historyDisplay.setItems(history);	
+	}
+	
+	private void callHistoryOnClick() {
+		historyDisplay.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle (MouseEvent click) {
+			Parser parser = new Parser(interpreter);
+			String repeatCommand = historyDisplay.getSelectionModel().getSelectedItem();
+			parser.generateCommandTree(
+					repeatCommand, myResources);
+			interpreter.addToHistory(repeatCommand);
+			}
+		});
 	}
 }
 
