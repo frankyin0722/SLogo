e7383159793318219288f42a9f01ac0eff9ce898
Shicheng Rao <sr307@duke.edu>
2018-03-10 00:55:25 -0500

    commented draggable image view

--- src/turtle/DraggableImageView.java
+++ src/turtle/DraggableImageView.java
@@ -2,10 +2,18 @@ package turtle;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-
+/**
+ * improved form of imageview, allowes for dragging
+ * specifically, dragging it is possible, as well as double clicking allowing for rotation
+ */
 public class DraggableImageView extends ImageView {
     private double mouseX;
     private double mouseY;
+    /**
+     * makes a draggable image with given turtle and image
+     * @param turtle
+     * @param image
+     */
     public DraggableImageView(Turtle turtle, Image image) {
         super(image);
         this.setOnMouseClicked(e -> {
