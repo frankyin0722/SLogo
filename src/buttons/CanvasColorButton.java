82f05501be7b34da4b3a5b7020e5c7fbea688138
Elizabeth Shulman <ers58@duke.edu>
2018-03-10 02:05:53 -0500

    completed JavaDocs

--- src/buttons/CanvasColorButton.java
+++ src/buttons/CanvasColorButton.java
@@ -6,22 +6,40 @@ import javafx.scene.control.ColorPicker;
 import javafx.stage.Stage;
 import view.canvas.DrawingWindow;
 
+/**
+ * 
+ * @author elizabethshulman
+ * 
+ * This class enables a user to change the color of the standard canvas,
+ * upon which the turtles move
+ */
 public class CanvasColorButton extends BaseButton {
 
 	private DrawingWindow currentCanvas;
 	
+	/**
+	 * Create instance of ColorCanvasButton, using
+	 * @param interpreter to access the necessary TurtleController and canvas.
+	 */
 	public CanvasColorButton(CommandTreeInterpreter interpreter) {
 		super("Change Canvas Color");
 		currentCanvas = interpreter.getTurtleController().getDrawingWindow();
 		this.setOnAction(e -> initializeWindow());
 	}
 
+	/**
+	 * Calls for Stage creation, using ColorPicker as a root node.
+	 */
 	private void initializeWindow() {
 		Stage colorSelection = new Stage();
 		colorSelection.setScene(new Scene(initializeColorPicker()));
 		colorSelection.show();
 	}
 
+	/**
+	 * Builds the ColorPicker and sets color selection to change canvas color
+	 * @return ColorPicker to change the canvas color
+	 */
 	private ColorPicker initializeColorPicker() {
 		ColorPicker colorPicker = new ColorPicker();
 		colorPicker.setOnAction(e -> currentCanvas.setBackgroundColor(colorPicker.getValue()));
--- src/buttons/LanguageChangeButton.java
+++ src/buttons/LanguageChangeButton.java
@@ -12,12 +12,24 @@ import javafx.stage.Stage;
 import slogo_team08.IConstants;
 import view.vis_elements.Visualization;
 
+/**
+ * 
+ * @author elizabethshulman
+ * 
+ * This class enables a user to change the language of inputted commands. It creates a button,
+ * which upon click opens a new pop-up list of buttons (one representing each available language).
+ * 
+ */
 public class LanguageChangeButton extends Button implements IConstants {
 
 	private ResourceBundle myResources;
 	private Visualization vis;
 	Stage languageView;
 
+	/**
+	 * Initialize a LanguageChangeButton, creating a new pop-up window upon click
+	 * @param visualization 	the Visualization object that requires knowledge of language changes
+	 */
 	public LanguageChangeButton(Visualization visualization) {
 		vis = visualization;
 		setLanguage(DEFAULT_LANGUAGE);
@@ -30,12 +42,20 @@ public class LanguageChangeButton extends Button implements IConstants {
 		this.setMaxWidth(Double.MAX_VALUE);
 	}
 	
+	/**
+	 * Presents a list of buttons with the JavaFX ListView layout
+	 * @return 	ListView presentation of buttons
+	 */
 	private ListView<Button> setupButtonView() {
 		ListView<Button> buttonDisplay = new ListView<>();
 		buttonDisplay.setItems(FXCollections.observableArrayList(setupAllButtons()));
 		return buttonDisplay;
 	}
 
+	/**
+	 * Traverses through resource files and creates one new button for each valid language
+	 * @return	list of buttons, each representing a different language
+	 */
 	private ArrayList<Button> setupAllButtons() {
 		ArrayList<Button> buttonList = new ArrayList<>();
 		File languageFile = new File(DEFAULT_LANGUAGE_FOLDER);
@@ -47,6 +67,11 @@ public class LanguageChangeButton extends Button implements IConstants {
 		return buttonList;
 	}
 	
+	/**
+	 * Initializes a language-specific button, setting the IDE's language to change upon selection
+	 * @param language
+	 * @return button with initiated event handlers
+	 */
 	private BaseButton setupButton(String language) {
 		BaseButton button = new BaseButton(language);
 		button.setOnAction(e -> {
@@ -56,11 +81,19 @@ public class LanguageChangeButton extends Button implements IConstants {
 		return button;
 	}
 	
+	/**
+	 * Accesses Visualization's languages bundle to enable other IDE objects to change language (e.g. parser)
+	 * @param language 	Selected language to be used in reading commands
+	 */
 	private void setLanguage(String language) {
 		myResources = ResourceBundle.getBundle(LANGUAGE_RESOURCE_PACKAGE + language);
 		vis.setLanguage(language);
 	}
 	
+	/**
+	 * Retrieves myResources, the current LanguageBundle to be used in reading commands
+	 * @return ResourceBundle containing language information
+	 */
 	public ResourceBundle getLanguage() {
 		return myResources;
 	}
