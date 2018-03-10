d0b6ca15db3c229a35ceb099840ee7bfc1d6f801
xy72 <xy72@duke.edu>
2018-03-10 01:00:44 -0500

    Added view.menu and view.canvas documentation

--- src/view/menus/ColorPaletteMenu.java
+++ src/view/menus/ColorPaletteMenu.java
@@ -21,33 +21,54 @@ import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import slogo_team08.IConstants;
-
+/**
+ * Menu to handle default color and color changes
+ * Default colors read from properties files
+ * Users can thus reference colors directly in code
+ * @author elizabethshulman
+ *
+ */
 public class ColorPaletteMenu extends TitledPane implements IConstants {
 
 	private ResourceBundle myResources;
 	private HashMap<Integer, Paint> indexToColor;
 	private CommandTreeInterpreter interpreter;
-	
+	/**
+	 * Constructor takes
+	 * @param i, interpreter
+	 * to later initialize this menu in interpreter
+	 */
 	public ColorPaletteMenu(CommandTreeInterpreter i) {
 		this(i, DEFAULT_COLOR_PALETTE);
 	}
-		
+	/**
+	 * Constructor takes
+	 * @param i, interpreter
+	 * @param colorPalette, default color palette
+	 */
 	public ColorPaletteMenu(CommandTreeInterpreter i, String colorPalette) {
 		interpreter = i;
 		initializeFormat(colorPalette);
 	}
-	
+	/**
+	 * Method visualizes default colors
+	 * Initialized menu
+	 * @param colorPalette
+	 */
 	private void initializeFormat(String colorPalette) {
+		this.setText("Default Color Options");
+		this.setExpanded(false);
 		try {
 			myResources = ResourceBundle.getBundle(COLOR_RESOURCE_PACKAGE + colorPalette);
 			buildMenu();
 		} catch (MissingResourceException e) {
 			new Alert(AlertType.ERROR, "Invalid color properties file: Check resource location and name", ButtonType.OK).showAndWait();
 		}
-		this.setText("Default Color Options");
-		this.setExpanded(false);
 	}
-	
+	/**
+	 * Helper function initializes menu that maps default indicies to 
+	 * default colors. Colors are visualized.
+	 */
 	private void buildMenu() {
 		ArrayList<HBox> myColorOptions = new ArrayList<>();
 		indexToColor = new HashMap<Integer, Paint>();
@@ -66,7 +87,14 @@ public class ColorPaletteMenu extends TitledPane implements IConstants {
 		colorDisplay.setItems(FXCollections.observableArrayList(myColorOptions));
 		this.setContent(colorDisplay);
 	}
-	
+	/**
+	 * Helper function aligns chosen
+	 * @param key, stated color name
+	 * @param index, index of color
+	 * @return HBox that visualizes color shade and index
+	 * Will be called for every color
+	 * Main purpose for graphic organization
+	 */
 	private HBox buildHbox(String key, int index) {
 		HBox colorOption = new HBox();
 		Rectangle colorSample = new Rectangle(15,15);
@@ -75,7 +103,10 @@ public class ColorPaletteMenu extends TitledPane implements IConstants {
 				new Text("   " + key + " (" + Integer.toString(index) + ")"));
 		return colorOption;
 	}
-	
+	/**
+	 * Method to
+	 * @return current color for each index
+	 */
 	public Map<Integer,Paint> getIndexToColorMap() {
 		return indexToColor;
 	}
--- src/view/menus/UserCommandsMenu.java
+++ src/view/menus/UserCommandsMenu.java
@@ -3,7 +3,6 @@ package view.menus;
 import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
-
 import interpreter.CommandTreeInterpreter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -14,28 +13,50 @@ import javafx.scene.input.MouseEvent;
 import observables.Listener;
 import parser.CommandNode;
 import view.vis_elements.CommandWindow;
-
+/**
+ * Menu that stores user created commands
+ * Clicking on command will send the command to CommandWindow
+ * where user can edit parameters and run command
+ * @author xlany, elizabethshulman
+ *
+ */
 public class UserCommandsMenu extends TitledPane implements Listener {
 	private 	CommandTreeInterpreter interpreter;
 	private ListView<String> commandsDisplay;
 	private CommandWindow myCommandWindow;
-
+	/**
+	 * Constructor that gets
+	 * user defined commands from @param i
+	 * languages from @param resources
+	 * sends commands upon click to @param cw
+	 */
 	public UserCommandsMenu(CommandTreeInterpreter i, ResourceBundle resources, CommandWindow cw) {
-		setupMenu();
-		commandsDisplay = new ListView<>();
 		interpreter = i;
 		myCommandWindow = cw;
-		i.addUDCListener(this);
-		this.setContent(commandsDisplay);
+		setupMenu();
+		setupListeners();
 		setupMouseEventHandler(myCommandWindow);
 	}
-	
+	/**
+	 * Method to initialize menu
+	 */
 	private void setupMenu() {
-//		this.setText(resources.getString(HISTORY_KEY));
+		commandsDisplay = new ListView<>();
 		this.setText("User Defined Commands");
 		this.setExpanded(false);
+		this.setContent(commandsDisplay);
 	}
-	
+	/**
+	 * Method adds this class as listener for updates in 
+	 * user defined commands in interpreter
+	 */
+	private void setupListeners() {
+		interpreter.addUDCListener(this);
+	}
+	/**
+	 * Method that sends command name and parameters to
+	 * @param cw CommandWindow upon click
+	 */
 	private void setupMouseEventHandler(CommandWindow cw) {
 		commandsDisplay.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
@@ -47,13 +68,21 @@ public class UserCommandsMenu extends TitledPane implements Listener {
 			}
 		});
 	}
-
+	/**
+	 * Listener method: updates when interpreter has new user commands
+	 * Displays the new commands in menu
+	 */
 	@Override
 	public void update() {
 		ObservableList<String> names = FXCollections.observableArrayList(interpreter.getUserCommands().keySet());
 		commandsDisplay.setItems((ObservableList<String>) names);
 	}
-	
+	/**
+	 * Helper method to turn
+	 * @param list of parameters into 
+	 * @return string
+	 * Formatted for user to run in CommandWindow
+	 */
 	private String paraToString(List<CommandNode> list) {
 		String s = "";
 		for (CommandNode c: list) {
@@ -61,5 +90,4 @@ public class UserCommandsMenu extends TitledPane implements Listener {
 		}
 		return s.substring(0, s.length()-1) + "\n";
 	}
-
 }
